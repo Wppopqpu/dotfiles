47a0378171300b60162508b87b4b929ba71b30ce
Wppopqpu <wppopqpu@outlook.com>
2024-05-25 09:10:08 +0800

    update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 	fish_vi_key_bindings
 	abbr -a fm firefox --new-window "file://$(pwd)"
+	abbr -a stow stow -v2
+	abbr -a rstow sudo stow -t / -v2
 end
