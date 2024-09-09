4da83f29a39d79b1687d496280341ac6c51b532d
Wppopqpu <wppopqpu@outlook.com>
2024-09-09 08:50:27 +0800

    feat: add fcitx config

---
+++ fcitx/.bashcfg/fcitx.bash
@@ -0,0 +1,7 @@
+export XMODIFIERS="@im=fcitx"
+export QT_IM_MODULE=fcitx
+export GTK_IM_MODULE=fcitx
+export XIM=fcitx
+export XIM_PROGRAM=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=fcitx
