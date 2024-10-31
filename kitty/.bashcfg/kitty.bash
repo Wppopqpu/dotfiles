6a88e44bc7fea0b896c40a3f0383ac28590423fb
Wppopqpu <wppopqpu@outlook.com>
2024-10-31 19:48:54 +0800

    feat: update kitty config

--- kitty/.bashcfg/kitty.bash
+++ kitty/.bashcfg/kitty.bash
@@ -2,10 +2,16 @@
 FPATH=~/.local/share/nvim/lazy/tokyonight.nvim/extras/kitty/tokyonight_moon.conf
 TARGET=~/.config/kitty/theme.conf
 
-# echo testing theme path
+echo Testing kitty theme path.
 if [[ -f $FPATH ]] ; then
+	SRC_DATE=$(stat -c %Y $FPATH)
+	DST_DATE=$(stat -c %Y $TARGET)
+	if [[ $DST_DATE -ge $SRC_DATE ]] ; then
+		echo Kitty theme already updated. Skipped.
+		exit 0
+	fi
 	cp -f $FPATH $TARGET
-	# echo "Kitty theme fetched from tokyonight."
+	echo Kitty theme fetched from tokyonight.
 else
 	echo "" > $TARGET
 fi
