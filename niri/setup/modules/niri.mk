dcff233372bf728b6b85c891bc69604b76b7d64b
Wppopqpu <wppopqpu@outlook.com>
2026-02-19 17:35:19 +0800

    fix: no longer start xayland-satellite manually niri will start it automatically

--- niri/setup/modules/niri.mk
+++ niri/setup/modules/niri.mk
@@ -7,8 +7,10 @@ install-systemd-unit.niri:  $(NIRI_WANTS_DIR)/noctalia.service
 $(NIRI_WANTS_DIR)/noctalia.service: $(SYSTEMD_USER_DIR)/noctalia.service
 	ln -s $(SYSTEMD_USER_DIR)/noctalia.service $(NIRI_WANTS_DIR)/
 
-$(NIRI_WANTS_DIR)/xwayland-satellite.service: $(SYSTEMD_USER_DIR)/xwayland-satellite.service
-	ln -s $(SYSTEMD_USER_DIR)xwayland-satellite.service $(NIRI_WANTS_DIR)
+# no longer need to start xwayland-satellite manually in new niri versions
+# this is not used before, actually
+# $(NIRI_WANTS_DIR)/xwayland-satellite.service: $(SYSTEMD_USER_DIR)/xwayland-satellite.service
+	# ln -s $(SYSTEMD_USER_DIR)xwayland-satellite.service $(NIRI_WANTS_DIR)
 
 $(NIRI_WANTS_DIR):
 	mkdir -p $(NIRI_WANTS_DIR)
