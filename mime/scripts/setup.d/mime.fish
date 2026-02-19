3926cd82dfd7d20066083611d45d56a37fa1c039
Wppopqpu <wppopqpu@outlook.com>
2026-02-19 21:40:03 +0800

    feat: use zathura as application/pdf also some refactor

--- mime/scripts/setup.d/mime.fish
+++ mime/scripts/setup.d/mime.fish
@@ -2,24 +2,22 @@
 g_set_current_file mime.fish
 
 set NVIM_DESKTOP nvim.desktop
+set ZATHURA_DESKTOP org.pwmt.zathura.desktop
 
 g_log Setting up (g_quote xdg-mime)...
 
+# set_mime [desktop-file] [filetype]
 function set_mime
 	g_log Setting default of mimetype[s] (g_quote $argv[2..]) to (g_quote $argv[1])...
 	xdg-mime default $argv[1] $argv[2..]
 end
 
-function setup_nvim
-	g_log Setting up nvim...
+# setup_one_mime [filetype] [desktop-file]
+function setup_one_mime
+	g_log setting up (g_quote $argv[2]) for (g_quote $argv[1])
 
-	if test ! -x /usr/bin/xdg-mime
-		g_log (g_quote xdg-mime) does not exist. Skipping..
-		return
-	end
-
-	if test ! -f /usr/share/applications/$NVIM_DESKTOP
-		g_log Desktop file (g_quote $NVIM_DESKTOP) does not exist. Skipping...
+	if test ! -f /usr/share/applications/$argv[2]
+		g_log Desktop file (g_quote $argv[2]) does not exist. Skipping...
 		return
 	end
 
@@ -28,12 +26,23 @@ function setup_nvim
 		return
 	end
 
-	if test (xdg-mime query default text/plain) = nvim.desktop
-		g_log Default of (g_quote text/plain) is already set to (g_quote $NVIM_DESKTOP). Skipping...
+	if test (xdg-mime query default $argv[1]) = $argv[2]
+		g_log Default of (g_quote $argv[1]) is already set to (g_quote $argv[2]). Skipping...
 		return
 	end
 
-	set_mime $NVIM_DESKTOP text/plain
+	set_mime $argv[2] $argv[1]
 end
 
-setup_nvim
+function setup_mime
+
+	if test ! -x /usr/bin/xdg-mime
+		g_log (g_quote xdg-mime) does not exist. Skipping..
+		return
+	end
+
+	setup_one_mime text/plain $NVIM_DESKTOP
+	setup_one_mime application/pdf $ZATHURA_DESKTOP
+end
+
+setup_mime
