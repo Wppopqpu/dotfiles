b870e75b7a0c622351268af3e8c96e4fb958dec7
Wppopqpu <wppopqpu@outlook.com>
2026-02-22 11:14:27 +0800

    feat: use win2xcurtheme instead of win2xcur

--- cursor/.icons/koishi_cursors/README.md
+++ cursor/.icons/koishi_cursors/README.md
@@ -2,3 +2,5 @@ the files in original was extracted from the installer from
 [here](https://xk-downloads.ovso.top/), made by
 [普通的线框](https://space.bilibili.com/400839602),
 using `monodis`.
+
+file `original/install.inf` is modified to work with `win2xcurtheme`.
--- cursor/.icons/koishi_cursors/generate.fish
+++ cursor/.icons/koishi_cursors/generate.fish
@@ -2,51 +2,11 @@
 
 set SCALE 2
 
-mkdir -p res
 mkdir -p cursors
+rm -rf cursors/*
 
-# clean up
-rm res/*
-rm cursors/*
+cd original
 
-# use win2xcur to generate from .ani files
+win2xcurtheme ./install.inf --scale $SCALE -o ../cursors
 
-for each in original/*.ani
-	win2xcur $each --scale $SCALE -o res
-	echo generated from $each.
-end
-
-
-
-# create symlinks
-set -l mappings \
-    "Normal"     "default left_ptr arrow" \
-    "Help"       "help question_arrow" \
-    "Working"    "watch progress" \
-    "Busy"       "wait watch" \
-    "Text"       "text xterm" \
-    "Handwriting" "pencil" \
-    "Move"       "move" \
-    "Link"       "pointer hand hand2" \
-    "Unavailable" "not-allowed crossed_circle" \
-    "Vertical"   "ns-resize" \
-    "Horizontal" "ew-resize" \
-    "Diagonal1"  "nwse-resize" \
-    "Diagonal2"  "nesw-resize" \
-    "Pin"        "dnd-none grabbing"
-
-for i in (seq 1 2 (count $mappings))
-    set src $mappings[$i]
-    set links_str $mappings[(math $i + 1)]
-
-    if test -f "res/$src"
-        for linkname in (string split " " -- $links_str)
-            ln -sf "../res/$src" "cursors/$linkname"
-            echo "cursors/$linkname -> res/$src"
-        end
-    else
-        echo "WARN: 'res/$src' not exist, skipped."
-    end
-end
-
-echo done.
+cd ..
