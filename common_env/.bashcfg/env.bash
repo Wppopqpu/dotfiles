e16c732998df44538bb1203fa4353c62fb0a7308
Wppopqpu <wppopqpu@outlook.com>
2026-04-03 19:53:55 +0800

    chore: dont disable VK_KHR_present_wait

--- common_env/.bashcfg/env.bash
+++ common_env/.bashcfg/env.bash
@@ -3,4 +3,4 @@ export LC_COLLATE=C.UTF-8
 export PATH=$HOME/.local/bin:$PATH
 
 # 防止wayland下游戏冻结
-export VKD3D_DISABLE_EXTENSIONS=VK_KHR_present_wait
+# export VKD3D_DISABLE_EXTENSIONS=VK_KHR_present_wait
